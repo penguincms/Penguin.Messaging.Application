3cde47594e3a0cc2665402c4a0ffdd19a86f62be
anloffredo <anloffredo@massageenvy.com>
2020-03-07 01:29:57 -0700

    Auto Commit

--- Extensions/MessageBusExtensions.cs
+++ Extensions/MessageBusExtensions.cs
@@ -11,25 +11,37 @@ namespace Penguin.Messaging.Application.Extensions
         /// Sends a Startup message over the message bus
         /// </summary>
         /// <param name="messageBus">The message bus to use</param>
-        public static void Startup(this MessageBus messageBus) => messageBus.Send(new Penguin.Messaging.Application.Messages.Startup());
+        public static void Startup(this MessageBus messageBus)
+        {
+            messageBus.Send(new Penguin.Messaging.Application.Messages.Startup());
+        }
 
         /// <summary>
         /// Sends a Setup message over the message bus
         /// </summary>
         /// <param name="messageBus">The message bus to use</param>
-        public static void Setup(this MessageBus messageBus) => messageBus.Send(new Penguin.Messaging.Application.Messages.Setup());
+        public static void Setup(this MessageBus messageBus)
+        {
+            messageBus.Send(new Penguin.Messaging.Application.Messages.Setup());
+        }
 
         /// <summary>
         /// Sends a Shutdown message over the message bus
         /// </summary>
         /// <param name="messageBus">The message bus to use</param>
-        public static void Shutdown(this MessageBus messageBus) => messageBus.Send(new Penguin.Messaging.Application.Messages.Shutdown());
+        public static void Shutdown(this MessageBus messageBus)
+        {
+            messageBus.Send(new Penguin.Messaging.Application.Messages.Shutdown());
+        }
 
         /// <summary>
         /// Sends a Setup message over the message bus
         /// </summary>
         /// <typeparam name="T">The type of the object being set up</typeparam>
         /// <param name="messageBus">The message bus to use</param>
-        public static void Setup<T>(this MessageBus messageBus) => messageBus.Send(new Penguin.Messaging.Application.Messages.Setup<T>());
+        public static void Setup<T>(this MessageBus messageBus)
+        {
+            messageBus.Send(new Penguin.Messaging.Application.Messages.Setup<T>());
+        }
     }
 }
